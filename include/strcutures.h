7435965288d42d3b88fa230faa9ec8c707aa516e
Nath <mboulemouandjo@et.esiea.fr>
2025-05-05 14:16:27 +0200

    Ajout du .h des structures

---
+++ include/strcutures.h
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define taille_max 100
+
+typedef struct Bat{
+    char type[50];
+    int ID_Bat;
+    int etage;
+    int x;
+    int y;
+    int hauteur;
+    int age;
+    int capacite;
+    int etat;//allant de 0 à 100
+    ressources bat_stock;
+}Bat;
+
+typedef struct ressources{
+    int quantite;
+    char type[50];
+}ressources;
+
+typedef struct Summit_city{
+    int ID;
+    char type[50];
+    char nom[50];
+    int population;
+    Bat* batiment;
+    
+}Summit_city;
+
+typedef struct Tab_sommets{
+    Summit_city* sommets;
+    int n_sommets;
+    Bat* batiments;
+    int n_bats;
+}Tab_sommets;
+
+typedef struct Graphe{
+    
+    Summit_city* sommets;
+    Tab_sommets* sommets_TAB;
+    Route chemins[taille_max][taille_max];
+
+}Graphe;
+
+typedef struct Route{
+
+    int distance;
+    int etat;
+    int capacité;
+}Route;
