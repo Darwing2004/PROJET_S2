463bd01474311fd3700a9d39fe471b0b133626ee
Nath <mboulemouandjo@et.esiea.fr>
2025-05-09 20:57:20 +0200

    Modification de Projet_C_mission1

---
+++ include/structures.h
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define taille_max 100
+
+typedef struct Bat{
+    char type[50];
+    char etat_courant[50];
+    int ID_Bat;
+    int etage;
+    int x;
+    int y;
+    int hauteur;
+    int age;
+    int capacite;
+    int etat;//allant de 0 à 100
+    ressources* bat_stock;
+}Bat;
+
+typedef struct ressources{
+    int quantite;
+    char type[50];
+}ressources;
+
+typedef struct Sommet_ville{
+    int ID;
+    char type[50];
+    char nom[50];
+    int population;
+    Bat* batiment;
+
+}Sommet_ville;
+
+typedef struct Tab_sommets{
+    Sommet_ville* sommets;
+    int n_sommets;
+    Bat* batiments;
+    int n_bats;
+}Tab_sommets;
+
+typedef struct Graphe{
+    
+    Sommet_ville* sommets;
+    Tab_sommets* sommets_TAB;
+    Route chemins[taille_max][taille_max];
+
+}Graphe;
+
+typedef struct Route{
+
+    int distance;
+    int etat;
+    int capacité;
+    char etat_actuel[50];
+}Route;
